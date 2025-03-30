8312022555640dbf9450fc0b59af7265d6e7a00e
Ordinarykid-oo <russeldaniel23@gmail.com>
2025-03-30 19:01:13 +0800

    Add files via upload

---
+++ orca_share_media1743332437794_7312066217170905918.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  
+   //dictionary of fruits and vegetables from A - Z 
+    
+    string a;
+    
+    cout << "Enter Letter: ";
+    cin >> a;
+    
+    if (a == "A")(
+    cout << "Apple");
+    else if (a == "B")(
+    cout << "Banana");
+    else if (a == "C")(
+    cout << "Carrot");
+    else if (a == "D")(
+    cout << "Durian");
+    else if (a == "E")(
+    cout << "Eggfruit");
+    else if (a == "F")(
+    cout << "Feijoa");
+    else if (a == "G")(
+    cout << "Garlic");
+    else if (a == "H")(
+    cout << "Honeydew");
+    else if (a == "I")(
+    cout << "Iyokan");
+    else if (a == "J")(
+    cout << "Jackfruit");
+    else if (a == "K")(
+    cout << "Kiwi");
+    else if (a == "L")(
+    cout << "Lemon");
+    else if (a == "M")(
+    cout << "Mango");
+    else if (a == "N")(
+    cout << "Nuts");
+    else if (a == "O")(
+    cout << "Orange");
+    else if (a == "P")(
+    cout << "Potato");
+    else if (a == "Q")(
+    cout << "Quince");
+    else if (a == "R")(
+    cout << "Radish");
+    else if (a == "S")(
+    cout << "Squash");
+    else if (a == "T")(
+    cout<< "Tomato");
+    else if (a == "U")(
+    cout << "Ube");
+    else if (a == "V")(
+    cout << "Velvet apple");
+    else if (a == "W")(
+    cout << "Walnuts");
+    else if (a == "X")(
+    cout << "Xing zi");
+    else if (a == "Y")(
+    cout << "Yellow pepper");
+    else if (a == "Z")(
+    cout << "Zebra tomato");
+
+    return 0;
+}
